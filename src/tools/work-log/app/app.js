081a891485c4ba73a264e9096bce53bdf81b3d25
JUNOTA <junnoota@gmail.com>
2026-08-24 20:17:48 +0900

    Update tools 2026-08-24 11:17

--- src/tools/work-log/app/app.js
+++ src/tools/work-log/app/app.js
@@ -118,6 +118,12 @@ const monthlyPlanAddCategoryButton =
   );
 
 
+const monthlyPlanSaveButton =
+  document.getElementById(
+    "monthly-plan-save-button"
+  );
+
+
 const workTimeLabel =
   document.getElementById(
     "work-time-label"
@@ -1136,7 +1142,8 @@ function updatePeriodButtons() {
 function initializeMonthlyPlan() {
   if (
     !monthlyPlanForm ||
-    !monthlyPlanMonthInput
+    !monthlyPlanMonthInput ||
+    !monthlyPlanSaveButton
   ) {
     return;
   }
@@ -1307,15 +1314,14 @@ function initializeMonthlyPlan() {
   );
 
 
-  monthlyPlanForm.addEventListener(
-    "submit",
+  monthlyPlanSaveButton.addEventListener(
+    "click",
 
-    function(event) {
-      event.preventDefault();
-
+    function() {
       saveMonthlyPlan();
     }
   );
+
 
   copyPreviousPlanButton.addEventListener(
     "click",
