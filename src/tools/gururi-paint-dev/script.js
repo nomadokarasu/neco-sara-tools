41545b1794efc2ff0acbff1bdfe6864334480e51
JUNOTA <junnoota@gmail.com>
2026-09-08 14:54:09 +0900

    Update tools 2026-09-08 05:54

--- src/tools/gururi-paint-dev/script.js
+++ src/tools/gururi-paint-dev/script.js
@@ -19,7 +19,7 @@ const scene = new THREE.Scene();
 ================================ */
 
 const APP_VERSION =
-"1.3.81";
+"1.3.82";
 
 
 const appVersion =
@@ -16441,7 +16441,7 @@ window.addEventListener(
 () => {
 
 navigator.serviceWorker.register(
-"./service-worker.js?v=1.3.81",
+"./service-worker.js?v=1.3.82",
 {
 updateViaCache: "none"
 }
