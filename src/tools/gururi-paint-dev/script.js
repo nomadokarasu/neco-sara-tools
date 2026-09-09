8d98d1b4d346cec557b7c41e247ed5edc758ec15
JUNOTA <junnoota@gmail.com>
2026-09-09 21:55:29 +0900

    Update tools 2026-09-09 12:55

--- src/tools/gururi-paint-dev/script.js
+++ src/tools/gururi-paint-dev/script.js
@@ -64,7 +64,7 @@ const scene = new THREE.Scene();
 ================================ */
 
 const APP_VERSION =
-"1.3.108";
+"1.3.109";
 
 
 const appVersionElements =
@@ -17899,7 +17899,7 @@ window.addEventListener(
 () => {
 
 navigator.serviceWorker.register(
-"./service-worker.js?v=1.3.108",
+"./service-worker.js?v=1.3.109",
 {
 updateViaCache: "none"
 }
--- src/tools/gururi-paint-dev/service-worker.js
+++ src/tools/gururi-paint-dev/service-worker.js
@@ -2,7 +2,7 @@ const CACHE_PREFIX =
 "gururi-paint-dev-";
 
 const CACHE_VERSION =
-"1.3.108";
+"1.3.109";
 
 const CACHE_NAME =
 `${CACHE_PREFIX}${CACHE_VERSION}`;
@@ -11,8 +11,8 @@ const STATIC_RESOURCES = [
 "./",
 "./index.html",
 "./home-content.json",
-"./style.css?v=1.3.108",
-"./script.js?v=1.3.108",
+"./style.css?v=1.3.109",
+"./script.js?v=1.3.109",
 "./images/welcome-ja.png",
 "./images/welcome-en.png",
 "./images/gururi-paint-hero.png",
