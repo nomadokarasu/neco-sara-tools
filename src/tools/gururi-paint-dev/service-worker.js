fc7a7d1466565ac6d96a314e5b031b84840c50cb
JUNOTA <junnoota@gmail.com>
2026-09-09 20:49:21 +0900

    Update tools 2026-09-09 11:49

--- src/tools/gururi-paint-dev/service-worker.js
+++ src/tools/gururi-paint-dev/service-worker.js
@@ -2,7 +2,7 @@ const CACHE_PREFIX =
 "gururi-paint-dev-";
 
 const CACHE_VERSION =
-"1.3.101";
+"1.3.102";
 
 const CACHE_NAME =
 `${CACHE_PREFIX}${CACHE_VERSION}`;
@@ -11,8 +11,8 @@ const STATIC_RESOURCES = [
 "./",
 "./index.html",
 "./home-content.json",
-"./style.css?v=1.3.101",
-"./script.js?v=1.3.101",
+"./style.css?v=1.3.102",
+"./script.js?v=1.3.102",
 "./images/welcome-ja.png",
 "./images/welcome-en.png",
 "./images/gururi-paint-hero.png",
